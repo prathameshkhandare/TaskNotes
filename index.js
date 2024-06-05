8c320815c60ce4c792d5375e83e104e40bba0590
prathmesh_khandare <prathameshkhandare1551@gmail.com>
2024-06-05 20:37:06 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+app.use(express.json());
+
+app.use(express.urlencoded({ extended:true}));
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function(req, res){
+    fs.readdir(`./files` , function(err, files){
+     
+        if(err) {
+            console.log(err);
+            return;
+        }
+        else{
+       
+       res.render('index',{files:files});
+        }
+    })
+    
+
+    
+})
+
+
+app.post('/create', function(req, res){
+    const fileName = req.body.title.split(' ').join('') + '.txt';
+    fs.writeFile(`./files/${fileName}`, req.body.details, function(err) {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log('file created');
+            res.redirect('/');
+        }
+    });
+});
+
+app.get('/files/:fileName', function(req, res){
+
+fs.readFile(`./files/${req.params.fileName}`,'utf-8', function(err,filedata) {
+    if (err) {
+        console.log(err);
+        return;
+    } else {
+        res.render('task',{fileName:req.params.fileName,filedata:filedata});
+    }
+})
+
+
+})
+
+const port =4000;
+
+app.listen(port, ()=>{
+    console.log('listening on port ' + port);
+})
