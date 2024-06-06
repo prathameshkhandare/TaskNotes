ce0c9c0bc9a891f7a9f2fa5dd26a0c8cd2682dbc
prathmesh_khandare <prathameshkhandare1551@gmail.com>
2024-06-06 17:25:01 +0530

    prepared vercel.json , index.js for vercel deployment

--- index.js
+++ index.js
@@ -2,34 +2,26 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
-app.use(express.json());
 
-app.use(express.urlencoded({ extended:true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views')); // Corrected path for views
+app.use(express.static(path.join(__dirname, '../public'))); // Corrected path for public
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.get('/', function(req, res){
-    fs.readdir(`./files` , function(err, files){
-     
-        if(err) {
+app.get('/', function(req, res) {
+    fs.readdir('./files', function(err, files) {
+        if (err) {
             console.log(err);
             return;
+        } else {
+            res.render('index', { files: files });
         }
-        else{
-       
-       res.render('index',{files:files});
-        }
-    })
-    
+    });
+});
 
-    
-})
-
-
-app.post('/create', function(req, res){
+app.post('/create', function(req, res) {
     const fileName = req.body.title.split(' ').join('') + '.txt';
     fs.writeFile(`./files/${fileName}`, req.body.details, function(err) {
         if (err) {
@@ -42,41 +34,36 @@ app.post('/create', function(req, res){
     });
 });
 
-app.get('/files/:fileName', function(req, res){
-
-fs.readFile(`./files/${req.params.fileName}`,'utf-8', function(err,filedata) {
-    if (err) {
-        console.log(err);
-        return;
-    } else {
-        res.render('task',{fileName:req.params.fileName,filedata:filedata});
-    }
-})
-
-
-})
-
-app.get('/edit/:fileName', function(req, res){
-    res.render('edit',{fileName:req.params.fileName});
-})
-
-app.post('/edit', function(req, res){
-
-    // console.log(req.body);
-    fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,function(err){
-        if(err){
+app.get('/files/:fileName', function(req, res) {
+    fs.readFile(`./files/${req.params.fileName}`, 'utf-8', function(err, filedata) {
+        if (err) {
             console.log(err);
             return;
+        } else {
+            res.render('task', { fileName: req.params.fileName, filedata: filedata });
         }
-        else{
-            res.redirect('/');
-        }
-    })
+    });
 });
 
+app.get('/edit/:fileName', function(req, res) {
+    res.render('edit', { fileName: req.params.fileName });
+});
 
-const port =4000;
+app.post('/edit', function(req, res) {
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, function(err) {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/');
+        }
+    });
+});
 
-app.listen(port, ()=>{
+const port = 4000;
+
+app.listen(port, () => {
     console.log('listening on port ' + port);
-})
+});
+
+module.exports = app;
