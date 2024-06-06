ea9135ceb6d70710e64103ce9e8c53dc2b8cae27
prathmesh_khandare <prathameshkhandare1551@gmail.com>
2024-06-06 16:41:45 +0530

    added edit filename option

--- index.js
+++ index.js
@@ -56,6 +56,25 @@ fs.readFile(`./files/${req.params.fileName}`,'utf-8', function(err,filedata) {
 
 })
 
+app.get('/edit/:fileName', function(req, res){
+    res.render('edit',{fileName:req.params.fileName});
+})
+
+app.post('/edit', function(req, res){
+
+    // console.log(req.body);
+    fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,function(err){
+        if(err){
+            console.log(err);
+            return;
+        }
+        else{
+            res.redirect('/');
+        }
+    })
+});
+
+
 const port =4000;
 
 app.listen(port, ()=>{
